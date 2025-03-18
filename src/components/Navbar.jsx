7a1ec2792f8ed0ef51eedefeff853c10c736cdd2
SophiaSaks <82449876+SophiaSaks@users.noreply.github.com>
2025-03-18 14:59:05 +0100

    WIP

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,22 @@
 import './Navbar.css'
 import logoWhite from '../assets/logo_white.png'
+import burgerWhite from '../assets/hamburger_white.png'
+import profileWhite from '../assets/hamburger_white'
 
 function Navbar() {
   return (
     <div className="Navbar">
 
+      <div className="burger">
+        <img src={burgerWhite} />
+      </div>
+
       <div className="logo">
         <img src={logoWhite} />
+      </div>
+
+      <div className="profile">
+        <img src={profileWhite} />
       </div>
       
     </div>
