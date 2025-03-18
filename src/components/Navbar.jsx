ac99a4958bb27b2a75c4423602d092da031758b6
SophiaSaks <82449876+SophiaSaks@users.noreply.github.com>
2025-03-18 14:45:15 +0100

    WIP

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,13 @@
 import './Navbar.css'
+import logoWhite from '../assets/logoWhite'
 
 function Navbar() {
   return (
     <div className="Navbar">
+      <div className="logo">
+        <img src={logoWhite} />
+
+      </div>
         <h1>Hi</h1>
     </div>
   );
